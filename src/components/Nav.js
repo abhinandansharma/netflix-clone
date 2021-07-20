d042279e5ecaff90677036a1d7fa8752035a4e85
Abhinandan Sharma <abhinandan@Abhinandans-MacBook-Air.local>
2021-07-20 17:56:05 +0530

    update navbar with new categories and icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,6 @@
 import React,{useState,useEffect} from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 import "./Nav.css";
 
 const Nav= ()=> {
@@ -20,11 +22,24 @@ const Nav= ()=> {
     
     return (
         <nav className={`nav ${show && "nav__black"}`}>
+            {/* left side of the nav bar */}
             <img
                 alt="Netflix logo"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
                 className="nav__logo"
             />
+            <div className="nav-container">
+                <h4 id="activeCategory">Home</h4>
+                <h4 className="category">TV Shows</h4>
+                <h4 className="category">Movies</h4>
+                <h4 className="category">Recently Added</h4>
+                <h4 className="category">My List</h4>
+            </div>
+            {/* right side of the nav bar */}
+            <div className="navIcon-container">
+                <SearchIcon className="navIcon"/>
+                <NotificationsIcon className="navIcon"/>
+            </div>
             <img
                 alt="User logged"
                 src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
